920f1c450687bf204f1fd591889d02fec9d15193
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2021-11-10 23:26:08 +0000

    Add configuration of language.

--- src/ruddocom.policy/src/ruddocom/policy/setuphandlers.py
+++ src/ruddocom.policy/src/ruddocom/policy/setuphandlers.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
+import logging
+
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from plone.app.multilingual.browser.setup import SetupMultilingualSite
+
+
+PROFILE = "ruddocom.policy:default"
+logger = logging.getLogger(PROFILE.split(":")[0]).warning
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
-
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
-            'ruddocom.policy:uninstall',
+            "ruddocom.policy:uninstall",
         ]
 
 
@@ -21,3 +28,12 @@ def post_install(context):
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+
+def setup_multilingual(context=None):
+    language_tool = api.portal.get_tool("portal_languages")
+    language_tool.addSupportedLanguage("es")
+    language_tool.addSupportedLanguage("en-us")
+    setuptool = SetupMultilingualSite()
+    portal = api.portal.get()
+    setuptool.setupSite(portal)
