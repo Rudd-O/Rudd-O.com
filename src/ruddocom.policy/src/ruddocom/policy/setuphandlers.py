1ab44316a29dcda2825ead4bc9211dcf453cfd16
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2021-11-11 13:06:05 +0000

    Fix activation problem.

--- src/ruddocom.policy/src/ruddocom/policy/setuphandlers.py
+++ src/ruddocom.policy/src/ruddocom/policy/setuphandlers.py
@@ -10,7 +10,7 @@ from plone.app.redirector.interfaces import IRedirectionStorage
 
 
 PROFILE = "ruddocom.policy:default"
-logger = logging.getLogger(PROFILE.split(":")[0]).warning
+logger = logging.getLogger(__name__).warning
 
 
 @implementer(INonInstallable)
@@ -24,7 +24,8 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    setup_multilingual(context)
+    setup_language_folder_redirects(context)
 
 
 def uninstall(context):
@@ -39,6 +40,7 @@ def setup_multilingual(context=None):
     setuptool = SetupMultilingualSite()
     portal = api.portal.get()
     setuptool.setupSite(portal)
+    logger("Done setting up multilingual")
 
 
 def setup_language_folder_redirects(context=None):
@@ -53,3 +55,4 @@ def setup_language_folder_redirects(context=None):
         root = phys_path[:-len(path)]
         phys_redirect = root + redirect
         storage.add(phys_redirect, phys_path)
+    logger("Done setting up language redirects")
