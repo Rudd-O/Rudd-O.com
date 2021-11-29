3a1b35c0adb629d0d3b6c7165ddcab70cdff0586
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2021-11-29 22:02:34 +0000

    Add exporter to Plone 5, which does not support content export (I cannot get it to work).

--- src/ruddocom.policy/ruddocom/policy/__init__.py
+++ src/ruddocom.policy/ruddocom/policy/__init__.py
@@ -1,3 +1,15 @@
 # -*- extra stuff goes here -*-
 
+import os
 import ruddocom.policy.workarounds
+
+
+def _func(fname, adjustedzope2cmd, unused_extra_cmdline_args):
+    embedded = os.path.join(os.path.dirname(__file__), "%s_embedded.py" % fname)
+    adjustedzope2cmd.options.program += [fname]
+    adjustedzope2cmd.options.args = ["-c", embedded] + adjustedzope2cmd.options.args[1:]
+    return adjustedzope2cmd.do_run("")
+
+
+def export(adjustedzope2cmd, unused_extra_cmdline_args):
+    return _func("export", adjustedzope2cmd, unused_extra_cmdline_args)
--- src/ruddocom.policy/setup.py
+++ src/ruddocom.policy/setup.py
@@ -43,5 +43,6 @@ setup(name='ruddocom.policy',
 
       [plone.recipe.zope2instance.ctl]
       upgrade = ruddocom.policy.upgrader:main
+      export = ruddocom.policy:export
       """,
       )
