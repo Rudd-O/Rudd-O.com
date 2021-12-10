1831e79ebe8ea3a932557cd0b5b6d8a87aa8abb9
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2021-12-10 00:50:47 +0000

    Basic schema-based picture with custom view.

--- src/ruddocom.policy/src/ruddocom/policy/content/picture.py
+++ src/ruddocom.policy/src/ruddocom/policy/content/picture.py
@@ -1,8 +1,24 @@
-# -*- coding: utf-8 -*-
-from plone.dexterity.content import Item
+from plone.app.multilingual.dx.interfaces import ILanguageIndependentField
+from plone.namedfile import field
 from plone.supermodel import model
+from zope import schema
+from zope.interface import alsoProvides
 
 
 class IPicture(model.Schema):
-    """ Marker interface for Picture
-    """
+    """Marker interface for Picture"""
+
+    picture = field.NamedBlobImage(
+        description="The picture",
+        title="Picture",
+    )
+
+    source = schema.URI(
+        description="The source for this picture",
+        title="Source",
+        required=False,
+    )
+
+
+alsoProvides(IPicture["picture"], ILanguageIndependentField)
+alsoProvides(IPicture["source"], ILanguageIndependentField)
