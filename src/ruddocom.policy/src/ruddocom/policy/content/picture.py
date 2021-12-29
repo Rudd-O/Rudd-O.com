8fa2124c91451ed64e2a49a671e1b64ab4e643e8
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2021-12-29 01:26:26 +0000

    My first adapter -- from picture to ifeeditem, to let the stupid Twitter cards work.

--- src/ruddocom.policy/src/ruddocom/policy/content/picture.py
+++ src/ruddocom.policy/src/ruddocom/policy/content/picture.py
@@ -1,8 +1,11 @@
+from Products.CMFPlone.browser.syndication.adapters import DexterityItem
+from Products.CMFPlone.interfaces.syndication import IFeed
 from plone.app.multilingual.dx.interfaces import ILanguageIndependentField
 from plone.dexterity.content import Item
 from plone.namedfile import field
 from plone.supermodel import model
 from zope import schema
+from zope.component import adapts
 from zope.interface import alsoProvides
 
 
@@ -30,3 +33,29 @@ class Picture(Item):
     @property
     def image(self):
         return self.picture
+
+
+class PictureAdapter(DexterityItem):
+    adapts(IPicture, IFeed)
+
+    @property
+    def has_enclosure(self):
+        return True
+
+    @property
+    def file_length(self):
+        return self.context.picture.size
+
+    @property
+    def file_type(self):
+        return self.context.picture.contentType
+
+    @property
+    def file_url(self):
+        return "{}/@@display-file/picture/{}".format(self.base_url, self.context.picture.filename)
+
+    guid = file_url
+
+    @property
+    def file(self):
+        return self.context.picture
